fe396e96815b27e61cf3e1c071b7cda2a3d8a15a
1478952 <1478952@naver.com>
2023-11-28 22:24:44 +0900

    Node.js 15일차 검증 이해

---
+++ userInputAndValidation/routes/auth.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const { check, body } = require("express-validator");
+
+const authController = require("../controllers/auth");
+const User = require("../models/user");
+
+const router = express.Router();
+
+router.get("/login", authController.getLogin);
+
+router.get("/signup", authController.getSignup);
+
+router.post(
+  "/login",
+  [
+    check("email")
+      .isEmail()
+      .withMessage("Please enter a valid email")
+      .normalizeEmail(),
+    body("password", "please enter a password with only numbers")
+      // 숫자만 허용
+      .isLength({ min: 5 })
+      .isAlphanumeric()
+      .trim(),
+  ],
+  authController.postLogin
+);
+
+router.post(
+  "/signup",
+  [
+    check("email")
+      .isEmail()
+      .withMessage("Please enter a valid email")
+      .custom((value) => {
+        return User.findOne({
+          email: value,
+        }).then((userDoc) => {
+          if (userDoc) {
+            return Promise.reject(
+              "E-mail exists already, please pick a different one."
+            );
+          }
+        });
+      })
+      .normalizeEmail(),
+    body("password", "please enter a password with only numbers")
+      // 숫자만 허용
+      .isLength({ min: 5 })
+      .isAlphanumeric()
+      .trim(),
+    body("confirmPassword")
+      .trim()
+      .custom((value, { req }) => {
+        // 동등성검사
+
+        if (value !== req.body.password) {
+          throw new Error("password have to match!");
+        }
+        return true;
+      }),
+  ],
+  authController.postSignup
+);
+
+router.post("/logout", authController.postLogout);
+
+router.get("/reset", authController.getReset);
+
+router.post("/reset", authController.postReset);
+
+router.get("/reset/:token", authController.getNewPassword);
+
+router.post("/new-password", authController.postNewPassword);
+
+module.exports = router;
